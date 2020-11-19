99aca5363e8096e44845f7d05c36deb1f0c94fb2
langkye <langkye@gmail.com>
2020-11-20 03:27:47 +0800

    封装统一返回结果

--- blogs-java/src/main/java/com/langkye/controller/UserController.java
+++ blogs-java/src/main/java/com/langkye/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.langkye.controller;
 
 
+import com.langkye.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
+    @GetMapping("/test")
+    public Object test(){
+        return userService.getById(1l);
+    }
 }
